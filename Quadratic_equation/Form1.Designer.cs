8346e3a2ce777dd06ac2d60f03e7e8f1b71a8e4d
Ilya Toritsyn <toritsyn41@gmail.com>
2020-04-23 11:57:50 +0300

    Добавьте файлы проекта.

---
+++ Quadratic_equation/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Quadratic_equation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.label_a = new System.Windows.Forms.Label();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.label_b = new System.Windows.Forms.Label();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.button_solve = new System.Windows.Forms.Button();
+            this.label_answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(13, 13);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 20);
+            this.textBox_a.TabIndex = 0;
+            this.textBox_a.Enter += new System.EventHandler(this.TextBox_a_Enter);
+            this.textBox_a.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_a_KeyPress);
+            // 
+            // label_a
+            // 
+            this.label_a.AutoSize = true;
+            this.label_a.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_a.Location = new System.Drawing.Point(120, 13);
+            this.label_a.Name = "label_a";
+            this.label_a.Size = new System.Drawing.Size(52, 21);
+            this.label_a.TabIndex = 1;
+            this.label_a.Text = "x^2 +";
+            this.label_a.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(182, 13);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(100, 20);
+            this.textBox_b.TabIndex = 2;
+            this.textBox_b.Enter += new System.EventHandler(this.TextBox_b_Enter);
+            this.textBox_b.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_b_KeyPress);
+            // 
+            // label_b
+            // 
+            this.label_b.AutoSize = true;
+            this.label_b.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_b.Location = new System.Drawing.Point(289, 13);
+            this.label_b.Name = "label_b";
+            this.label_b.Size = new System.Drawing.Size(35, 21);
+            this.label_b.TabIndex = 3;
+            this.label_b.Text = "x +";
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Location = new System.Drawing.Point(332, 13);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(100, 20);
+            this.textBox_c.TabIndex = 4;
+            this.textBox_c.TextChanged += new System.EventHandler(this.TextBox_c_TextChanged);
+            this.textBox_c.Enter += new System.EventHandler(this.TextBox_c_Enter);
+            this.textBox_c.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_c_KeyPress);
+            // 
+            // button_solve
+            // 
+            this.button_solve.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_solve.Location = new System.Drawing.Point(13, 40);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(419, 33);
+            this.button_solve.TabIndex = 5;
+            this.button_solve.Text = "Решить";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label_answer
+            // 
+            this.label_answer.AutoSize = true;
+            this.label_answer.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_answer.Location = new System.Drawing.Point(13, 80);
+            this.label_answer.Name = "label_answer";
+            this.label_answer.Size = new System.Drawing.Size(0, 21);
+            this.label_answer.TabIndex = 6;
+            this.label_answer.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 114);
+            this.Controls.Add(this.label_answer);
+            this.Controls.Add(this.button_solve);
+            this.Controls.Add(this.textBox_c);
+            this.Controls.Add(this.label_b);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.label_a);
+            this.Controls.Add(this.textBox_a);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.Label label_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.Label label_b;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.Button button_solve;
+        private System.Windows.Forms.Label label_answer;
+    }
+}
+
---
+++ Quadratic_equation/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Quadratic_equation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Проверка числа на валидность.
+        /// </summary>
+        /// <param name="textBox">Обрабатываемый текстбокс.</param>
+        /// <param name="coefficient">Коэффициент из текстбокса.</param>
+        /// <returns>true - валидно, false - невалидно</returns>
+        private bool IsValidNumber(TextBox textBox, ref double coefficient)
+        {
+            try
+            {
+                coefficient = Convert.ToDouble(textBox.Text);
+
+                return true;
+            }
+
+            catch
+            {
+                textBox.BackColor = Color.LightCoral;
+
+                return false;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            bool isOK = true; //Переменная-флаг для проверки ввода.
+
+            //Проверяем, валидные ли значения введены.
+            isOK = IsValidNumber(textBox_a, ref a) && isOK;
+            isOK = IsValidNumber(textBox_b, ref b) && isOK;
+            isOK = IsValidNumber(textBox_c, ref c) && isOK;
+
+            //Если ввод верный, решаем уравнение.
+            if (isOK)
+            {
+                if (a != 0)
+                {
+                    double discriminant = b * b - 4 * a * c;
+
+                    if (discriminant < 0)
+                    {
+                        label_answer.Text = "Корней нет.";
+                        label_answer.Visible = true;
+                    }
+
+                    else if (discriminant == 0)
+                    {
+                        double x = (-b / (2 * a));
+
+                        label_answer.Text = "Корень равен " + x + ".";
+                        label_answer.Visible = true;
+                    }
+
+                    else if (discriminant > 0)
+                    {
+                        double x1 = ((-b - Math.Sqrt(discriminant)) / (2 * a));
+                        double x2 = ((-b + Math.Sqrt(discriminant)) / (2 * a));
+
+                        label_answer.Text = "1 корень равен " + x1 + ". " +
+                        "2 корень равен " + x2 + ".";
+                        label_answer.Visible = true;
+                    }
+                }
+
+                else
+                {
+                    if (b != 0)
+                    {
+                        label_answer.Text = $"Ответ: x = {-c / b}.";
+                        label_answer.Visible = true;
+                    }
+
+                    else if (c != 0)
+                    {
+                        textBox_c.BackColor = Color.LightCoral;
+
+                        label_answer.Text = $"{c} = 0 - неверное равенство.";
+                        label_answer.Visible = true;
+                    }
+
+                    else if (c == 0)
+                    {
+                        label_answer.Text = $"x - любое число.";
+                        label_answer.Visible = true;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Значения коэффициентов по умолчанию.
+            textBox_a.Text = "1";
+            textBox_b.Text = "1";
+            textBox_c.Text = "0";
+        }
+
+        /// <summary>
+        /// Разрешение ввода лишь цифр, '-' в начале и 1 запятой после цифры.
+        /// </summary>
+        /// <param name="textBox">Обрабатываемый текстбокс.</param>
+        /// <param name="e">Нажатая клавиша.</param>
+        private void IsValidKey(TextBox textBox, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            char charBeforeCursor; //Символ до курсора.
+
+            try
+            {
+                charBeforeCursor = textBox.Text[textBox.SelectionStart - 1];
+            }
+
+            catch
+            {
+                charBeforeCursor = ' ';
+            }
+
+            //Игнорирование неподходящих символов при вводе коэффициентов.
+            if (!Char.IsDigit(number))
+            {
+                //Разрешаем печатать '-' в начале.
+                if (number == '-' && textBox.SelectionStart == 0)
+                {
+
+                }
+
+                //Разрешаем печатать лишь 1 запятую и лишь после цифры.
+                else if (number == ',' && (Char.IsDigit(charBeforeCursor) && !textBox.Text.Contains(',')))
+                {
+
+                }
+
+                //Разрешаем пользоваться Backspace.
+                else if (number == 8)
+                {
+
+                }
+
+                //Игнорируем все непредусмотренные символы.
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+
+            //Разрешаем печатать цифры.
+            else
+            {
+
+            }
+        }
+
+        private void TextBox_a_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            IsValidKey(textBox_a, e);
+        }
+
+        private void TextBox_b_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            IsValidKey(textBox_b, e);
+        }
+
+        private void TextBox_c_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            IsValidKey(textBox_c, e);
+        }
+
+        private void TextBox_c_Enter(object sender, EventArgs e)
+        {
+            textBox_c.BackColor = Color.White;
+            label_answer.Visible = false; //Скрываем сообщение об ошибке.
+        }
+
+        private void TextBox_a_Enter(object sender, EventArgs e)
+        {
+            textBox_a.BackColor = Color.White;
+        }
+
+        private void TextBox_b_Enter(object sender, EventArgs e)
+        {
+            textBox_b.BackColor = Color.White;
+        }
+
+        private void TextBox_c_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
